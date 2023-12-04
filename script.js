1dc754377b51d0265cf85ad938d0b22c920b0703
Rose <rosebeats09@gmail.com>
2023-12-03 21:26:08 -0500

    Fix build tree function

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-/* eslint-disable max-classes-per-file */
+// A binary tree node
+// eslint-disable-next-line max-classes-per-file
 class Node {
-  constructor(data, left, right) {
+  constructor(data) {
     this.data = data;
     this.left = null;
     this.right = null;
   }
 }
+
 class Tree {
   constructor(array, root) {
     this.array = array;
   }
+
+  buildTree(array, start, end) {
+    const mid = parseInt((start + end) / 2);
+    const node = new Node(array[mid]);
+    node.left = this.buildTree(array, start, mid - 1);
+    node.right = this.buildTree(array, mid + 1, end);
+    return node;
+  }
 }
-const array1 = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+const array1 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 console.log(array1);
