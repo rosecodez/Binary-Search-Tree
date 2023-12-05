0c2a2ebb65b546fb64703959fd11c9cd7f621f18
Rose <rosebeats09@gmail.com>
2023-12-05 18:53:00 -0500

    Make function to create data and insert it in the node

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ class Node {
 }
 
 class Tree {
-  constructor(array, root) {
-    this.array = array;
+  constructor(root) {
+    this.root = null;
   }
 
   buildTree(array, start, end) {
@@ -20,7 +20,43 @@ class Tree {
     node.right = this.buildTree(array, mid + 1, end);
     return node;
   }
+
+  // method that creates a new node to be inserted
+  insertData(data) {
+    const newNode = new Node(data);
+    // if root is null
+    if (this.root === null) {
+      // add node to the tree and make root
+      this.root = newNode;
+      // otherwise call insert node
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  // method to insert node in tree
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      // if left is null
+      if (node.left === null) {
+        // insert here
+        node.left = newNode;
+      } else {
+        // if left is not null, recur until null is found
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      // if right is null
+    } if (node.right === null) {
+      // insert node here
+      node.right = newNode;
+    } else {
+      // if right is not null, recur until null is found
+      this.insertNode(node.right, newNode);
+    }
+  }
 }
+// eslint-disable-next-line no-unused-vars
 const prettyPrint = (node, prefix = '', isLeft = true) => {
   if (node === null) {
     return;
@@ -28,10 +64,21 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
   if (node.right !== null) {
     prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
   }
+  // eslint-disable-next-line no-console
   console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
   if (node.left !== null) {
     prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
   }
 };
-const array1 = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-console.log(array1);
+const bst = new Tree();
+bst.insertData(1);
+bst.insertData(5);
+bst.insertData(9);
+bst.insertData(14);
+bst.insertData(26);
+bst.insertData(33);
+bst.insertData(47);
+bst.insertData(164);
+bst.insertData(5921);
+// eslint-disable-next-line no-console
+console.log(bst);
